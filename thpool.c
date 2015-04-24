1ead4c3e15c7cf6d5e6a5ad0dde87c3b85bace97
Alex Kir <alex@wall-dev.org>
2015-04-24 11:49:45 +0800

    fixed identation by spaces from previous commit

--- thpool.c
+++ thpool.c
@@ -337,8 +337,8 @@ static void* thread_do(struct thread* thread_p){
 	/* Register signal handler */
 	struct sigaction act;
 	act.sa_handler = thread_hold;
-    act.sa_flags = 0;
-    sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
 	if (sigaction(SIGUSR1, &act, NULL) == -1) {
 		fprintf(stderr, "thread_do(): cannot handle SIGUSR1");
 	}
